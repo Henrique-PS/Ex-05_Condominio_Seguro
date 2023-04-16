0ea9b17159fa1ee868318181fcaf221e691e2498
Henrique <103683674+Henrique-PS@users.noreply.github.com>
2023-04-16 19:46:29 -0300

    Update class Endereco

--- src/Endereco.java
+++ src/Endereco.java
@@ -4,18 +4,37 @@ public class Endereco {
     private String numero;
     private String apartamento;
 
+
+    //#region Contrutores
+
+    /**
+     * Cria um novo endereço para uma Casa
+     * @param rua Rua onde a casa se localiza
+     * @param bairro Bairro onde a casa se localiza
+     * @param numero Número da casa
+     */
     public Endereco(String rua, String bairro, String numero) {
         this.rua = rua;
         this.bairro = bairro;
         this.numero = numero;
     }
 
+    /**
+     * Cria um novo endereço para um Apartamento
+     * @param rua Rua onde o apartamento se localiza
+     * @param bairro Bairro onde o apartamento se localiza
+     * @param numero Número do prédio do apartamento
+     * @param apartamento Número do apartamento
+     */
     public Endereco(String rua, String bairro, String numero, String apartamento) {
         this.rua = rua;
         this.bairro = bairro;
         this.numero = numero;
         this.apartamento = apartamento;
     }
+    //#endregion
+
+    //#region getters
 
     public String getRua() {
         return rua;
@@ -32,4 +51,5 @@ public class Endereco {
     public String getApartamento() {
         return apartamento;
     }
+    //#endregion
 }
